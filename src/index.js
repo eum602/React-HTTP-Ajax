a43a90991f65a28f8dd0eec73b7808e8b6e68499
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-18 11:57:19 -0500

    adding INTERCEPTORS from axios to execute code globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//this handles ALL OUTPUT REQUESTS
+axios.interceptors.request.use(request=>{//this will be available on all requests done by axios.
+    console.log(request)
+
+    //the main idea is to edit the request before sendinf it
+    //{add some modification to request}
+
+    //return the request otherwise you are blocking th request
+    return request
+}, error => { //this error handler applies when you do not have connecivity or things like that.
+    //handling error
+    console.log(error)
+    return Promise.reject(error)
+})
+//so every REQUEST will be handled by this interceptor
+
+//this handles ALL INCOMING RESPONSES
+axios.interceptors.response.use(response=>{
+    console.log(response)
+
+    return response
+}, error => {
+    console.log(error)
+
+    return Promise.reject(error)
+})
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
