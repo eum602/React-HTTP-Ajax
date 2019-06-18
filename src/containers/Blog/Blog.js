76615b27d958cca51605350eafde71a02d833a21
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-18 12:36:52 -0500

    Creating and using Axios Instances

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from '../../axios' //using my custom instances of axios
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
