7752bc532773becb25e0b799fec4d2801de5c960
Erick Pacheco Pedraza <eum602@gmail.com>
2019-06-25 23:43:00 -0500

    added a NavLink instead of Link from react-router-dom in order to set a DEFAULT class 'active' for an active anchor tag - l228

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import {Route, Link} from 'react-router-dom' //importing link to render instead of reloading
+import {Route, NavLink} from 'react-router-dom' //Using NavLink instead of Link,
+//because it allows us to manage styling classes
 import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {   
     
-    render () {        
+    render () {
         
-        return (            
+        return (
             <div className="Blog">
                 <div>
                     <header>
                         <nav>
                             <ul>
-                                <li><Link to="/">Home</Link></li>{/*simple Link optio */}
-                                <li><Link to={{//complex link options
+                                <li>
+                                    <NavLink 
+                                        to="/" 
+                                        exact                                        
+                                        >
+                                        Home
+                                    </NavLink></li>{/*By using
+                                    NavLink a new class called active is AUTOMATICALLY assigned
+                                    Also by using exact we tell react that the full path
+                                    should match with "/" in order to render this and so to set the anchor tag
+                                    with a class "active" by default*/}
+                                <li><NavLink to={{/*Similar to Link, NavLink allows for complex options
+                                    also there is no need to specify "exact" keyword because
+                                    here we do not have nested links which should not trigger the active class*/
                                     pathname:"/new-post",
                                     hash:"#submit",//a hash
                                     search:'?quick-submit=true'
-                                }}>New Post</Link></li>{/*http://localhost:3000/new-post?quick-submit=true#submit */}
+                                }}>New Post</NavLink></li>{/*http://localhost:3000/new-post?quick-submit=true#submit */}
                             </ul>
                         </nav>
                     </header>
@@ -32,3 +45,20 @@ class Blog extends Component {
 }
 
 export default Blog;
+
+/*
+Absolute path is always appended to your domain, so if you use: “/new-post” it means it is 
+attached to your ROOT domain www.example.com => www.example.com/new-post .
+Even if your base path is www.example.com/posts and you use “/new-post” the you wouldn’t 
+be redirected to www.example.com/posts/new-post instead you will be redirected to
+ your ROOT PATH => www.example.com/new-post  and posts will be ommited.
+
+When using "to" (<Link to={{//...}}) it is always threatened as an absolute path, so no matter if you put
+ “/new-post” or simply “new-post” it will be always redirected to your root path.
+
+To turn to a relative path you must use your path in a dynamyc way to set the final url:
+pathname:${this.props.match.url}`/new-post`,
+
+so we append the current full path to the direction we want to go. So if this.props,match.url is “www.example.com/posts” 
+then the final pathname will be: “www.example.com/posts/new-post”
+*/
